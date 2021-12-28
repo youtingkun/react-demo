78150a4154485d1b6b263f36747b2d4d5494b7e0
youtingkun <10816@ps.com>
2021-12-28 14:17:38 +0800

    feat: temp

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import {HashRouter ,Route,Switch,Redirect } from 'react-router-dom'
-import Login from './pages/login/index';
-import Home from './pages/home/index';
-import Dashboard from './pages/dashboard/index'
-import Layout from './components/layout/index'
-import NotFind from './pages/404/index';
-import ReduxDemo from './pages/redux-demo/index'
-
-
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { HashRouter, Route, Switch, Redirect } from "react-router-dom";
+import Login from "./pages/login/index";
+import Home from "./pages/home/index";
+import Dashboard from "./pages/dashboard/index";
+import Layout from "./components/layout/index";
+import NotFind from "./pages/404/index";
+import ReduxDemo from "./pages/redux-demo/index";
+import Temp from "./pages/temp/index";
 
 function App() {
   return (
@@ -29,24 +28,24 @@ function App() {
     //     </a>
     //   </header>
     // </div>
-      <HashRouter>
-          <Switch>
-            <Route path="/login" exact component={Login}></Route>
-            <Route path="/404" exact component={NotFind}></Route>
-            <Route path='/'  >
-                <Layout>
-                    <Switch>
-                        <Route path='/index' exact component={Dashboard}></Route>
-                        <Route path='/index/:name' exact component={Dashboard}></Route>
-                        <Route path="/home"  component={Home}></Route>
-                        <Route path='/redux' component={ReduxDemo}></Route>
-                        <Redirect from="*" to="/redux" />
-                    </Switch>
-                </Layout>
-            </Route>
-            
-          </Switch>
-      </HashRouter>
+    <HashRouter>
+      <Switch>
+        <Route path="/login" exact component={Login}></Route>
+        <Route path="/404" exact component={NotFind}></Route>
+        <Route path="/">
+          <Layout>
+            <Switch>
+              <Route path="/index" exact component={Dashboard}></Route>
+              <Route path="/index/:name" exact component={Dashboard}></Route>
+              <Route path="/home" component={Home}></Route>
+              <Route path="/redux" component={ReduxDemo}></Route>
+              <Route path="/temp" component={Temp}></Route>
+              <Redirect from="*" to="/redux" />
+            </Switch>
+          </Layout>
+        </Route>
+      </Switch>
+    </HashRouter>
   );
 }
 
---
+++ src/pages/temp/index.tsx
@@ -0,0 +1,54 @@
+import React from "react";
+import { Link, withRouter, RouteComponentProps } from "react-router-dom";
+interface IProps {}
+type HomeProps = IProps & RouteComponentProps;
+interface IState {}
+
+class Temp extends React.Component<HomeProps, IState> {
+  constructor(props: HomeProps) {
+    super(props);
+  }
+  state = {
+    count: 1,
+  };
+  componentDidMount() {
+    enum Color {
+      Red = 1,
+      Green = 2,
+      Blue = 4,
+    }
+    let colorName: string = Color[2];
+    console.log("colorName, Color.Red", Color[4], Color.Red);
+    const btnEle = document.getElementById("btn");
+    if (btnEle) {
+      btnEle && btnEle.addEventListener("click", this.handleClick);
+    }
+  }
+  handleClick = () => {
+    this.setState({
+      count: this.state.count + 1,
+    });
+    console.log(this.state.count); // 2
+
+    this.setState({
+      count: this.state.count + 1,
+    });
+    console.log(this.state.count); // 3
+
+    this.setState({
+      count: this.state.count + 1,
+    });
+    console.log(this.state.count); // 4,count最终为4
+  };
+
+  render() {
+    return (
+      <>
+        <button id="btn">触发原生事件</button>
+        <div>{this.state.count}</div>
+      </>
+    );
+  }
+}
+
+export default withRouter(Temp);
